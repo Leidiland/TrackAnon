2a63e8a109b4ab823c15c38e87c2ffc479ff2e14
Leidi <emil.leidland@gmail.com>
2026-03-09 00:57:43 +0100

    Initial commit

--- dp2/anonymizer/anonymizer.py
+++ dp2/anonymizer/anonymizer.py
@@ -13,7 +13,6 @@ from dp2.detection.structures import (
     PersonDetection,
 )
 from tops import logger
-#from dp2.stylemc import get_and_cache_direction, get_stylesW, init_affine_modules
 
 
 class Anonymizer:
@@ -55,23 +54,10 @@ class Anonymizer:
                 self.generator_cfgs[FaceDetection]
             )
             tops.logger.log(f"Loaded generator from: {face_G_cfg}")
-        # if car_G_cfg is not None:
-        #     self.generator_cfgs[VehicleDetection] = load_config(car_G_cfg)
-        #     self.generators[VehicleDetection] = build_trained_generator(
-        #         self.generator_cfgs[VehicleDetection]
-        #     )
-        #     tops.logger.log(f"Loaded generator from: {face_G_cfg}")
         self.dl = None
 
         self.prev_generated = {}
         self.prev_detected = None
-
-        # temp = np.load("known_identities.npz")
-        # self.known_ids = {
-        #     "embeddings": temp["embeddings"],
-        #     "labels": temp["labels"]
-        #     }
-        # self.creator = create_identities.IdentityCreator(self.detector)
 
     def initialize_tracker(self, fps: float):
         self.tracker = MultiObjectTracker(dt=1 / fps)
@@ -100,10 +86,6 @@ class Anonymizer:
         )
         batch["condition"] = batch["mask"].float() * batch["img"]
         cfg = self.generator_cfgs[detection_type]
-        # if text_prompt is not None and self.dl is None:
-        #     cfg.train.batch_size = 1
-        #     self.dl = tops.config.instantiate(cfg.data.val.loader)
-        #     init_affine_modules(G, batch)
         if z_idx is None:
             z_idx = np.random.randint(0, 2**32)
         state = np.random.RandomState(seed=z_idx)
@@ -122,20 +104,8 @@ class Anonymizer:
             )
         else:
             w = G.style_net.get_truncated(truncation_value, z=z)
-        
-        # if text_prompt is not None:
-        #     direction = get_and_cache_direction(cfg.output_dir, self.dl, G, text_prompt)
-        #     all_styles = get_stylesW(w)
-        #     all_styles = all_styles + direction * text_prompt_strength
-        #     all_styles = all_styles
-        # elif all_styles is not None:
-        #     all_styles = next(all_styles)
 
         with torch.amp.autocast('cuda'):
-            # if all_styles is not None:
-            #     anonymized_im = G(**batch, s=iter(all_styles))["img"]
-            # else:
-            #     anonymized_im = G(**batch, w=w)["img"]
             anonymized_im = G(**batch, w=w)["img"]
         anonymized_im = (anonymized_im + 1).div(2).clamp(0, 1).mul(255)
         return anonymized_im
@@ -179,16 +149,6 @@ class Anonymizer:
                 (y1 - y0, x1 - x0),
                 interpolation=F.InterpolationMode.NEAREST,
             ).squeeze(0)
-            # Previous generation
-            # if z_idx[idx] in self.prev_generated:
-            #     prev_patch = self.prev_generated[z_idx[idx]]
-            #     if prev_patch.shape != gim.shape:
-            #         prev_patch = F.resize(prev_patch, gim.shape[-2:], antialias=True)
-            #     alpha = 0.8  # weight for current frame
-            #     gim = (alpha * gim + (1 - alpha) * prev_patch).clamp(0, 255)
-            #
-            # # Update cache for this identity
-            # self.prev_generated[z_idx[idx]] = gim.detach().clone()
 
             # Remove padding
             pad = [max(-x0, 0), max(-y0, 0)]
@@ -204,13 +164,6 @@ class Anonymizer:
             x0, y0 = max(x0, 0), max(y0, 0)
             x1, y1 = min(x1, W), min(y1, H)
             mask = mask.logical_not()[None].repeat(3, 1, 1)
-
-            # im_patch = im[:, y0:y1, x0:x1].float()
-            # gim = gim.float()
-            # mask_f = (~mask).float()
-            # mask_f = F.gaussian_blur(mask_f, (9, 9), sigma=3)
-            # blended_patch = gim * (1 - mask_f) + im_patch * mask_f
-            # im[:, y0:y1, x0:x1] = blended_patch.round().clamp(0, 255).byte()
 
             im[:, y0:y1, x0:x1][mask] = gim[mask].round().clamp(0, 255).byte()
         return im
@@ -268,35 +221,12 @@ class Anonymizer:
             z_idx = np.array(z_idx)
             idx_offset = 0
 
-
-            # frame_bgr = im.permute(1, 2, 0).cpu().numpy()
-            # embeddings = [self.creator._extract_embedding(frame_bgr, box) for box in boxes]
-            #
-            # for i, emb in enumerate(embeddings):  # embeddings from current frame
-            #     assigned_id = self.match_to_known(emb, threshold=0.5)
-            #     if assigned_id is not None:
-            #         # assign a deterministic z value for anonymization
-            #         if assigned_id not in self.track_to_z_idx:
-            #             self.track_to_z_idx[assigned_id] = np.random.randint(0, 2 ** 32 - 1)
-            #         z_idx.append(self.track_to_z_idx[assigned_id])
-            #     else:
-            #         # fallback: new unknown identity
-            #         rand_z = np.random.randint(0, 2 ** 32 - 1)
-            #         z_idx.append(rand_z)
-            # z_idx = np.array(z_idx)
-
         # Meeting POV
-        target_z = [3071714933, 2588848963, 3638918503] #2678185683, bad --> 3626093760
+        target_z = [3071714933, 2588848963, 3638918503]
 
-        # CathalPOV
+        # Cathal POV
         # target_z = [2546248239, 3071714933, 3626093760, 2588848963, 3684848379,
         #             2340255427, 3638918503, 1819583497, 2678185683, 2774094101]
-
-        # Background --> foreground order (Tien: 3638918503)
-        # [2357136044 2546248239 3071714933 3626093760 2588848963 3684848379
-        #  2340255427 3638918503 1819583497 2678185683 2774094101]
-        # target_z = [self.track_to_z_idx[i] for i in [2, 5, 0, 6] if i in self.track_to_z_idx]
-
 
         for detection in all_detections:
             # Get z_idx and tracker IDs for the current batch
@@ -307,7 +237,7 @@ class Anonymizer:
                 current_ids = track_ids[idx_offset: idx_offset + len(detection)]
                 idx_offset += len(detection)
 
-            # --- Filter detections to only the target ---
+            # Filter detections to only the target
             no_target = True
             if no_target:
                 valid_idx = None
@@ -315,11 +245,9 @@ class Anonymizer:
                 valid_idx = []
                 for i, tid in enumerate(current_ids):
                     [valid_idx.append(i) for x in target_z if zs[i] == x]
-                # valid_idx = [i for i, z in enumerate(zs) if z in target_z]
 
                 if not valid_idx:
                     continue  # skip this detection batch if no target found
-
             im = self.anonymize_detections(im, detection, z_idx=zs, valid_idx=valid_idx, **synthesis_kwargs)
 
         return im.cpu()
